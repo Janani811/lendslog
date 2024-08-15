a62b366d9644114f4a8eb8546e798c65191f3fbc
janani811 <jananigopalsanthi@gmail.com>
2024-08-16 00:56:40 +0530

    empty state, refactoring, bug fixes, ui updates

--- app/(app)/dashboard/lends/(sub)/week.tsx
+++ app/(app)/dashboard/lends/(sub)/week.tsx
@@ -10,22 +10,22 @@ import { useAppSelector } from '@/redux/hooks';
 
 export default function week() {
   const { weekLends, isLoading } = useAppSelector(state => state.lends);
-    // Empty state
-    if (!isLoading && !weekLends.length) {
-      return (
-        <ThemedView
-          style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Emptystate
-            title="No Weekly lends found !"
-            description="Once you start lending for week base, all your weekly lends will appear in this space."
-          />
-        </ThemedView>
-      );
-    }
+  // Empty state
+  if (!isLoading && !weekLends.length) {
+    return (
+      <ThemedView
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Emptystate
+          title="No Weekly lends found !"
+          description="Once you start lending for week base, all your weekly lends will appear in this space."
+        />
+      </ThemedView>
+    );
+  }
   return (
     <ThemedView style={{ flex: 1 }}>
       <HeaderWithCount title="Week lends" count={weekLends.length} countText="lends" />
@@ -38,7 +38,7 @@ export default function week() {
           renderItem={({ item }: any) => {
             return (
               <LendsCard
-              ld_id={item.ld_id}
+                ld_id={item.ld_id}
                 ld_borrower_name={item.ld_borrower_name}
                 ld_payment_term={item.ld_payment_term}
                 ld_lend_amount={item.ld_lend_amount}
@@ -46,8 +46,8 @@ export default function week() {
                 ld_start_date={item.ld_start_date}
                 // ld_paid_amount= {item.ld_paid_amount}
                 // ld_pending_amount= {item.ld_pending_amount}
-                ld_paid_weeks= {item.ld_paid_weeks}
-                ld_pending_weeks= {item.ld_pending_weeks}
+                ld_paid_weeks={item.ld_paid_weeks}
+                ld_pending_weeks={item.ld_pending_weeks}
                 ld_total_weeks_or_month={item.ld_total_weeks_or_month}
                 ld_interest_amount={item.ld_interest_amount}
               />
